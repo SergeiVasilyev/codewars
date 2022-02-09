b57ed1302ea2fd6be77975726a23b0f682e68c6a
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-02-09 14:43:06 +0200

    is valid ip updated

--- is_valid_IP.py
+++ is_valid_IP.py
@@ -3,7 +3,7 @@ import re
 strng = '123.456.789.0'
 
 def is_valid_IP(strng):
-    answer = ''
+    answer = 0
     num = ''
     ip_num = {}
     idx = 0
@@ -12,18 +12,17 @@ def is_valid_IP(strng):
             num += el 
         else:
             num = ''
-            idx += 1
+            idx += 1 # сделать проверку на количество октетов
         ip_num[idx] = num
     print(ip_num)
     for x in ip_num:
         print(x)
-        if x < 0 or x > 255 or str(x)[0] == 0:
-            return False
-        else:
-            return True
-
-    print(answer)
-
+        if x >= 0 or x < 255 or str(x)[0] != 0:
+            answer ^= 1
+            print('x ', x)
+            print('answer ', answer)
+    
+    return bool(answer)
 
 
 print(is_valid_IP(strng))
