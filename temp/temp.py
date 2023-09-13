b5f4a149ab4630dcb50f363ddd785b0cf13a6830
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-09-13 23:07:56 +0300

    temp updated

--- temp/temp.py
+++ temp/temp.py
@@ -1,2 +1,10 @@
 # odd = True if not reduce(lambda a, b: a * b, integers[:3]) % 2 else False
 # odd = True if not reduce(lambda a, b: (a % 2) ^ (b % 2), integers[:3]) else False
+
+
+start = "10.0.0.0"
+
+st_ip = list(map(int, start.split(".")))
+st_ip_hex = list(map(lambda x: hex(x).split('x')[-1], st_ip))
+st_ip_hex2 = list(map(lambda x: hex(x), st_ip))
+print(st_ip_hex2[0] + st_ip_hex2[1], 0xa)
