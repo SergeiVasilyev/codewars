fd9cfcb3d4ca24ca09e8bd5c65597f92165f1088
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-09-14 09:59:39 +0300

    SECOND Solution: How many ip addresses between 2ip

--- Count IP Addresses/Count_ip.py
+++ Count IP Addresses/Count_ip.py
@@ -1,3 +1,4 @@
+# First solution. Convert all numbers to hex, combine and then get a difference between numbers
 def ips_between(start, end):
     st_ip = list(map(lambda x: hex(int(x)).split('x')[-1].zfill(2), start.split(".")))
     st_ip_hex_num = ''.join(st_ip)
@@ -7,10 +8,31 @@ def ips_between(start, end):
 
     return int(end_ip_hex_num, 16) - int(st_ip_hex_num, 16)
 
+# Second solution. Сalculate the decimal number by multiplying each byte by 256 to the power of the position of this byte
+# First byte 256**0, Second 256**1...
+def ips_between2(start, end):
+    start, end = start.split('.'), end.split('.')
+    sum_start, sum_end = 0, 0
+
+    # Make loop from 0 to 4
+    for n in range(0, 4):
+        sum_start += int(start[n]) * (256**abs(n-3)) # Reverse the power from 3 to 0
+        sum_end += int(end[n]) * (256**abs(n-3)) # Reverse the power from 3 to 0
+    
+    sum_ip_adresses = sum_end - sum_start
+
+    return sum_ip_adresses
+
 
 
 if __name__=='__main__':
-    print(ips_between("10.0.0.0", "10.0.0.50"))
-    print(ips_between("20.0.0.0", "20.0.2.0"))
-    print(ips_between("10.0.0.255", "10.0.1.0"))
+    print('First solution')
+    print("10.0.0.0 -", "10.0.0.50 =", ips_between("10.0.0.0", "10.0.0.50"))
+    print("20.0.0.0 -", "20.0.2.0 =", ips_between("20.0.0.0", "20.0.2.0"))
+    print("10.0.0.255 -", "10.0.1.0 =", ips_between("10.0.0.255", "10.0.1.0"))
+
+    print('\nSecond solution')
+    print("10.0.0.0 -", "10.0.0.50 =", ips_between2("10.0.0.0", "10.0.0.50"))
+    print("20.0.0.0 -", "20.0.2.0 =", ips_between2("20.0.0.0", "20.0.2.0"))
+    print("10.0.0.255 -", "10.0.1.0 =", ips_between2("10.0.0.255", "10.0.1.0"))
 
