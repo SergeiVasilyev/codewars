afe9e6f38c8543e30e1fdd14326281b8eef5da1e
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-09-27 10:58:32 +0300

    attempts updated

--- Sum of Intervals 4kyu/attempts/Sum of Intervals copy 2.py
+++ Sum of Intervals 4kyu/attempts/Sum of Intervals copy 2.py
@@ -11,23 +11,25 @@ def sum_of_intervals(intervals):
 
     for key, (start, end) in enumerate(intervals):
         print('key', key, start, end)
-        total3 = total.copy()
+        total3 = sorted(total.copy()) # For proper operation, the set must be sorted 
         print('total 3 init', total3)
         for y in total3:
             print('start, end, y', start, end, y[0], y[1])
-            if start in range(y[0]-1, y[1]+1) or end in range(y[0]-1, y[1]+1):
+            if y[0] <= start <= y[1] or y[0] >= end >= y[1]:
                 min = y[0] if y[0] < start else start
                 max = y[1] if y[1] > end else end
                 print('min max', min, max)
                 total.remove((y[0], y[1]))
-                total.add((min, max))
+                if (start, end) in total:
+                    total.remove((start, end))
+                total.add((min, max)) # Add() method don't add element to the end 
                 print('true', total)
             else:
                 print('else', total, '+', start, end)
                 total.add((start, end))
-        print('total', total)
 
-    return interval_list
+    s = sum(list(map(lambda x: x[1] - x[0], total)))
+    return total, s
 
 
 if __name__=='__main__':
@@ -36,4 +38,12 @@ if __name__=='__main__':
         [7, 10],
         [3, 5],
     ]
+    # 986
+    x = [(-493, 17), (-438, 385), (-159, -74), (-114, -23), (-73, 289), (-24, 465), (-22, 283), (69, 193), (111, 434), (159, 281), (185, 205), (192, 392), (196, 255), (203, 468), (249, 384), (261, 421), (300, 493), (359, 493)]
+    # 887
+    x = [(-485, 123), (-414, -200), (-372, 58), (-371, -98), (-330, 87), (-231, 8), (-22, 184), (42, 236), (134, 216), (320, 444), (368, 486), (403, 485), (435, 463)]
+    # 916
+    # x = [(-444, 144), (-320, -13), (-224, -133), (-146, -116), (-92, 387), (54, 339), (108, 472), (204, 420), (226, 316), (233, 408), (256, 410)]
+
+
     print(sum_of_intervals(x))
--- Sum of Intervals 4kyu/attempts/Sum of Intervals copy.py
+++ Sum of Intervals 4kyu/attempts/Sum of Intervals copy.py
@@ -1,5 +1,8 @@
 def sum_of_intervals(intervals):
     min, max = 0, 0
+    class ContinueI(Exception):
+        pass
+    continue_i = ContinueI()
 
     sorted_intervals = sorted(intervals, key=lambda x: x[0])
     print(sorted_intervals)
@@ -9,23 +12,31 @@ def sum_of_intervals(intervals):
         print('x', x)
         intervals2 = intervals.copy()
         intervals2.pop(i)
-        for n, y in enumerate(intervals2):
-            print('y', y)
-            if y[0] in range(x[0], x[1]+1) or y[1] in range(x[0], x[1]+1):
-                min = y[0] if y[0] < x[0] else x[0]
-                max = y[1] if y[1] > x[1] else x[1]
-                print('ints', intervals, intervals2)
-                if [y[0], y[1]] in intervals:
+        try:
+            for n, y in enumerate(intervals2):
+                print('y, x', y, x)
+                if x[0] <= y[0] <= x[1] or x[0] >= y[1] >= x[1]:
+                    min = y[0] if y[0] < x[0] else x[0]
+                    max = y[1] if y[1] > x[1] else x[1]
+                    print('ints', intervals, intervals2)
                     intervals.remove([y[0], y[1]])
-                if [min, max] in intervals:
-                    intervals.remove([min, max])
-                intervals.append([min, max])
-                print('add', [min, max])
-                print('remove y', y, intervals)
+                    if [x[0], x[1]] in intervals:
+                        intervals.remove([x[0], x[1]])
+                    if [min, max] in intervals:
+                        intervals.remove([min, max])
+                    intervals.append([min, max])
+                    print('add', [min, max])
+                    print('remove y', y, intervals)
+                    raise continue_i
+                else:
+                    if [x[0], x[1]] not in intervals:
+                        intervals.append([x[0], x[1]])
+        except ContinueI:
+            continue
 
-    print(sum(list(map(lambda x: x[1] - x[0], intervals))))
+    sum_intervals = (sum(list(map(lambda x: x[1] - x[0], intervals))))
 
-    return intervals
+    return intervals, sum_intervals
 
 
 if __name__=='__main__':
@@ -40,9 +51,10 @@ if __name__=='__main__':
         [7, 10],
         [1, 5],
     ]
-    # x = [[1, 5],
-    #     [10, 20],
-    #     [1, 6],
-    #     [16, 19],
-    #     [5, 11]]
+    x = [[1, 5],
+        [10, 20],
+        [1, 6],
+        [16, 19],
+        [5, 11]]
+    
     print(sum_of_intervals(x))
