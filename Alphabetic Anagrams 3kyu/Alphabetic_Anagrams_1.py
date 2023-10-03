6be283d62e821ec267f2bc8bc99bba3767f46c12
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-10-03 09:39:43 +0300

    Alphabetic_Anagrams 1 updated

--- Alphabetic Anagrams 3kyu/Alphabetic_Anagrams_1.py
+++ Alphabetic Anagrams 3kyu/Alphabetic_Anagrams_1.py
@@ -19,18 +19,6 @@ if __name__=='__main__':
     print(list_position('ABAB'))
     print(list_position('AAAB'))
     print(list_position('BAAA'))
-    # print(list_position('YTTSRONLIIAA'))
+    print(list_position('YTTSRONLIIAA'))
     print(list_position('QUESTION'))
     print(list_position('BOOKKEEPER'))
-
-# def list_position(st):
-#     sorted_string = sorted(st)
-#     st = list(st)
-#     k = 1
-#     for i in range(len(st)-1):
-#         for j in range(len(st)-i-1):
-#             if st[j] > st[j+1]:
-#                 print(st[j], st[i])
-#                 st[j], st[j+1] = st[j+1], st[j]
-#                 k += 1
-#     return st, k
