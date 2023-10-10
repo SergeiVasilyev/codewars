b8238161f6204585735c6288321e2ee61f9b4a88
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2023-10-10 13:02:27 +0300

    2 ver. works if there are no repeating characters

---
+++ Alphabetic Anagrams 3kyu/Alphabetic_Anagrams_2.py
@@ -0,0 +1,37 @@
+from icecream import ic
+from math import factorial
+from itertools import permutations
+import time
+
+# This version of the function works if there are no repeating characters in the word
+
+def list_position(word):
+    # Print all variantions using permutation function for check answer
+    # Don't use for long words
+    sorted_word = sorted(word)
+    perms = set(permutations(sorted_word))
+    ic(sorted(perms))
+
+    all_variants = factorial(len(word))
+    count = all_variants
+
+    for key, a in enumerate(word):
+        for b in word[key:]:
+            all_variants = factorial(len(word[key:]))
+            if a < b:
+                count -= all_variants/len(word[key:])
+                ic(count, all_variants, all_variants/len(word[key:]))
+    
+    return count
+
+
+
+if __name__=='__main__':
+    t0 = time.time()
+
+    ic(list_position('BAC'))
+    ic(list_position('BCDA'))
+
+    t1 = time.time()
+    code_speed = t1-t0
+    ic(code_speed)
